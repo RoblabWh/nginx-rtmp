43d955b4f2429ddddc83a1493768ba42c5ec39fe
Niklas Digakis <niklas.digakis@studmail.w-hs.de>
2022-08-05 13:17:56 +0200

    add live player

--- gen-live.sh
+++ gen-live.sh
@@ -4,6 +4,30 @@ HTMLFILE='/srv/streams.html'
 HTMLFILENEW='/tmp/streams.html'
 LIVEFILE='/tmp/livestreams'
 
+# generate html with videojs player
+gen_player()
+{
+  if [ -f "/srv/$1-$2.html" ]; then return; fi
+  printf "<!DOCTYPE html>
+  <html>
+  <head>
+    <title>$1 $2</title>
+    <link href=\"https://vjs.zencdn.net/7.20.1/video-js.css\" rel=\"stylesheet\" />
+  </head>
+  <body style=\"margin:0;\">
+    <video style=\"width:100vw;height:100vh;\" class=\"video-js\" controls autoplay muted preload=\"auto\" data-setup=\"{}\">
+        <source src=\"/$2/$1\" type=\"application/vnd.apple.mpegurl\" />
+        <p class=\"vjs-no-js\">
+            To view this video please enable JavaScript, and consider upgrading to a
+            web browser that
+            <a href=\"https://videojs.com/html5-video-support/\" target=\"_blank\">supports HTML5 video</a>
+        </p>
+    </video>
+    <script src=\"https://vjs.zencdn.net/7.20.1/video.min.js\"></script>
+  </body>
+  </html>" > "/srv/$1-$2.html"
+}
+
 gen_html()
 {
   STREAMS=$(cat "$LIVEFILE" 2>/dev/null)
@@ -32,11 +56,13 @@ gen_html()
     </tr>" > "$HTMLFILENEW"
 
   for i in $STREAMS; do
+    gen_player "$i" "live"
+    gen_player "$i" "lowres"
     printf "
     <tr>
       <td>$i</td>
-      <td><a href=\"/live/$i\">/live/$i</a></td>
-      <td><a href=\"/lowres/$i\">/lowres/$i</a></td>
+      <td><a href=\"/$i-live.html\">view</a></td>
+      <td><a href=\"/$i-lowres.html\">view</a></td>
     </tr>" >> "$HTMLFILENEW"
   done
 
@@ -46,6 +72,7 @@ gen_html()
   mv "$HTMLFILENEW" "$HTMLFILE"
 }
 
+# handle input
 if [ "$#" = "0" ]; then
   gen_html
 elif [ "$1" = '+' ]; then
